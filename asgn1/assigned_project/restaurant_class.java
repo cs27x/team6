b9f7af65727727e53175232ab063d8abf4284854
Alexander Wan <alexander.wan@vanderbilt.edu>
2014-09-19 15:09:05 -0500

    Completed methods, not tested yet

--- asgn1/assigned_project/restaurant_class.java
+++ asgn1/assigned_project/restaurant_class.java
@@ -1,32 +1,83 @@
 package restaurant_class;
 
-import java.sql.Time;
 import java.util.Date;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class restaurant_class {
 	
-	private final String name;
-	private final Boolean oncampus;
-	private final Boolean mealmoney;
-	private final float latitude;
-	private final float longitude;	
+	private final String name = "";
+	private final String typeoffood = "";
+	private final Boolean oncampus = false;
+	private final Boolean mealmoney = false;
+	private final Boolean delivers = false;
+	private final float latitude = 0;
+	private final float longitude = 0;	
 	
-	private final Time[] opentimes;
-	private final Time[] closingtimes;
+	Date date = new Date();
+	Calendar now = Calendar.getInstance();
+	private final int[] opentimes = new int [7];
+	private final int[] closingtimes = new int [7];
 
 	public boolean isOpen()
 	{
-		return true;
+		// Checks to see if restaurant is open now
+		int today = now.get(Calendar.DAY_OF_WEEK);
+		int openingtime = opentimes[today];
+		int closingtime = closingtimes[today];
+		
+		int from = openingtime;
+	    int to = closingtime;
+	    Date date = new Date();
+	    Calendar c = Calendar.getInstance();
+	    c.setTime(date);
+	    int t = c.get(Calendar.HOUR_OF_DAY) * 100 + c.get(Calendar.MINUTE);
+	    boolean isBetween = to > from && t >= from && t <= to || to < from && (t >= from || t <= to);
+		
+		return isBetween;
 	}
+	
+	public boolean isOpen(int day)
+	{
+		// Checks to see if restaurant is open at specified day
+				int openingtime = opentimes[day];
+				int closingtime = closingtimes[day];
+				
+				int from = openingtime;
+			    int to = closingtime;
+			    Date date = new Date();
+			    Calendar c = Calendar.getInstance();
+			    c.setTime(date);
+			    int t = c.get(Calendar.HOUR_OF_DAY) * 100 + c.get(Calendar.MINUTE);
+			    boolean isBetween = to > from && t >= from && t <= to || to < from && (t >= from || t <= to);
+				
+				return isBetween;
+	}
+	
+	public int[] getTimes()
+	{
+		// Returns an array of open and close times for today
+		// in integer format (military time)
+		
+		int[] times = new int [2];
+		
+		int today = now.get(Calendar.DAY_OF_WEEK);
+		times[0] = opentimes[today];
+		times[1] = closingtimes[today];
+		return times;
+	}
+	
+
 	
 	public boolean isMealPlan()
 	{
-		return true;
+		return mealmoney;
 	}
 	
 	public boolean hasDelivery()
 	{
-		return true;
+		return delivers;
 	}
 	
 	public boolean isonCampus()
@@ -36,24 +87,24 @@ public class restaurant_class {
 	
 	public String typeOfFood()
 	{
-		return "Chinese";
+		return typeoffood;
 	}
 	
-	public String getStartingHour(int day)
+	public int timeToClose()
 	{
-		String hour = "8 AM";
-		return hour;
-	}
-	
-	public String getClosingHour(int day)
-	{
-		String hour = "6 PM";
-		return hour;
-	}
-	
-	public Date timeToClose()
-	{
-		Date hours = null;
-		return hours;
+		if (isOpen())
+		{
+			int today = now.get(Calendar.DAY_OF_WEEK);
+			int closingtime = closingtimes[today];
+			
+		    int to = closingtime;
+		    Calendar c = Calendar.getInstance();
+		    c.setTime(date);
+		    int t = c.get(Calendar.HOUR_OF_DAY) * 100 + c.get(Calendar.MINUTE);
+		    int timetoclose = to - t;
+		    return timetoclose;
+		}
+		else
+			return 0;
 	}
 }
