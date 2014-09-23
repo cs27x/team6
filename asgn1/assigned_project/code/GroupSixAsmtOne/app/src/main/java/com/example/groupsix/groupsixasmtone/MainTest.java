e9a0af00f0c52294447022343d8ae2adb89ec429
Clark Perkins <r.clark.perkins@gmail.com>
2014-09-22 19:23:14 -0500

    Fixed MainTest.java to use singleton

--- asgn1/assigned_project/code/GroupSixAsmtOne/app/src/main/java/com/example/groupsix/groupsixasmtone/MainTest.java
+++ asgn1/assigned_project/code/GroupSixAsmtOne/app/src/main/java/com/example/groupsix/groupsixasmtone/MainTest.java
@@ -3,12 +3,13 @@ package com.example.groupsix.groupsixasmtone;
 
 /**
  * Created by clarkperkins on 9/20/14.
+ * 
  */
 public class MainTest {
 
     public static void main(String[] args) {
 
-        RestaurantList rl = new RestaurantList();
+        RestaurantList rl = RestaurantList.getInstance();
 
         for (Restaurant r : rl) {
             System.out.println(r.toString());
